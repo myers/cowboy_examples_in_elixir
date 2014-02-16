11bd2780277b5584305781bd7d62336b2c920b09
Myers Carpenter <myers@maski.org>
2014-02-15 21:09:14 -0500

    move where cowboy is started

--- bullet_clock/lib/bullet_clock.ex
+++ bullet_clock/lib/bullet_clock.ex
@@ -2,6 +2,21 @@ defmodule BulletClock do
   use Application.Behaviour
 
   def start(_type, _args) do
+    dispatch = [
+      {:_, [
+        {"/", :cowboy_static, {:priv_file, :bullet_clock, "static/index.html"}},
+        {"/bullet_js/[...]", :cowboy_static, {:priv_dir, :bullet, ""}},
+
+        {"/bullet", :bullet_handler, [{:handler, BulletClock.StreamHandler}]},
+
+        # catch all.  Must be last in the list
+        {"/[...]", :cowboy_static, {:priv_dir, :bullet_clock, "static"}},
+      ]}
+    ] |> :cowboy_router.compile
+
+    {:ok, _} = :cowboy.start_http(:http, 100, [port: 8080], [env: [dispatch: dispatch]])
+    IO.puts "Starting server http://localhost:8080/ ... (Ctrl-c, q, <enter> to stop)"
+    
     BulletClock.Supervisor.start_link
   end
 end
--- bullet_clock/lib/bullet_clock/supervisor.ex
+++ bullet_clock/lib/bullet_clock/supervisor.ex
@@ -2,24 +2,17 @@ defmodule BulletClock.Supervisor do
   use Supervisor.Behaviour
 
   def start_link do
-    dispatch = [
-      {:_, [
-        {"/", :cowboy_static, {:priv_file, :bullet_clock, "static/index.html"}},
-        {"/bullet_js/[...]", :cowboy_static, {:priv_dir, :bullet, ""}},
-
-        {"/bullet", :bullet_handler, [{:handler, BulletClock.StreamHandler}]},
-
-        # catch all.  Must be last in the list
-        {"/[...]", :cowboy_static, {:priv_dir, :bullet_clock, "static"}},
-      ]}
-    ] |> :cowboy_router.compile
-
-    {:ok, _} = :cowboy.start_http(:http, 100, [port: 8080], [env: [dispatch: dispatch]])
-    IO.puts "Starting server http://localhost:8080/ ... (Ctrl-c, q, <enter> to stop)"
-    :supervisor.start_link({:local, __MODULE__}, __MODULE__, [])
+    :supervisor.start_link(__MODULE__, [])
   end
 
   def init([]) do
-    {:ok, {{:one_for_one, 10, 10}, []}}
+    children = [
+      # Define workers and child supervisors to be supervised
+      # worker(BulletClock.Worker, [])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/Supervisor.Behaviour.html
+    # for other strategies and supported options
+    supervise(children, strategy: :one_for_one)
   end
 end
